59cadef58c98491318f10d398155c1d0863f1687
vasanthkumar <42891954+vasanth9@users.noreply.github.com>
2020-06-03 20:14:37 +0530

    hello php files wont execute

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+$json = $_SERVER["QUERY_STRING"] ?? '';
 $checkArray=array();
 array_push(($checkArray),"Hello","World,", "this", "is", "with", "HNGi7","ID","and" ,"email" , "using", "for" ,"stage" ,"2" ,"task");
 foreach(glob('scripts/*.js')as $filename){
@@ -11,9 +12,12 @@ foreach(glob('scripts/*.py')as $filename){
   $array[$filename]=$r;
 }
 $jsonarray=array();
+$pass=0;
+$total=sizeof($array);
 foreach($array as $key=>$value)
 {
   // Hello World, this is somto with HNGi7 ID HNG-04817 and email somto@gmail.com using Python for stage 2 task
+
 $x=explode(" ",$value);
 $length=sizeof($x);
 $m=0;
@@ -48,14 +52,17 @@ for($i=0;$i<$length;$i++)
 	{
 		$lang=$x[$i];
 	}
-
+     else
+     {
+     	$m++;
+     }
 
 }
 $status="Fail";
-//echo "<pre>$res</pre>";
+
 if($res==13)
 {
-$status="Pass";
+$status="Pass"; $pass++;
 }
  
 $newarray=array("file"=> "$file","output"=> "$output","name"=>"$name","id"=> "$id","email"=> "$email","language"=> "$lang","status"=> "$status");
@@ -64,5 +71,93 @@ array_push(($jsonarray),$newarray);
 
 }
 $myJSON = json_encode($jsonarray);
-echo $myJSON;
+
+ob_end_flush();
+
+    if (isset($json) && $json == 'json') {
+        echo $myJSON;
+    } else {
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WONDER WOMAN</title>
+	<style>
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th, td {
+  padding: 8px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+}
+.tt
+{
+	background-color: green;
+}
+.tti
+{
+	background-color: red;
+
+}
+</style>
+</head>
+<body>
+<h2>total<?php echo ": $total"?></h2>
+<h2>pass<?php echo ": $pass"?></h2>
+<h2>fail<?php $fail=$total-$pass;echo ":$fail "?></h2>
+
+
+<table>
+  <tr><th>sno</th>
+    <th>file</th>
+    <th>Name</th>
+    <th>OUTPUT</th>
+  <th>Status</th>
+  </tr>
+  <?php
+  $d=0;
+foreach($jsonarray as $key=>$value)
+{$rr=array();$d++;
+	array_push($rr,$jsonarray[$key]);
+  $o=$rr[0]["file"];
+	$t=$rr[0]["name"];
+    $e=$rr[0]["output"];
+    $s=$rr[0]["status"];
+    if($e=="")
+    {$e="Nothing is printed";
+    $t="nothing";
+      echo " <tr class='tti'>
+    <td>$d</td>
+    <td>$o</td>
+    <td>$t</td>
+    <td>$e</td>
+    <td>$s</td>
+  </tr>";
+    }else{
+    if($s=="Pass"){
+ echo " <tr class='tt'>
+<td>$d</td>
+    <td>$o</td>
+    <td>$t</td>
+    <td>$e</td>
+    <td>$s</td>
+  </tr>";}
+  else
+  {
+  	echo " <tr class='tti'>
+    <td>$d</td>
+    <td>$o</td>
+    <td>$t</td>
+    <td>$e</td>
+    <td>$s</td>
+  </tr>";
+  }
+}}
+  ?>
+</table>
+</body>
+</html>
+<?php } ?>
